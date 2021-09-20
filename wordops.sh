3d38968bd28e1a88a8f2072287d48341ed425f90
Oberon Lai <m615926@gmail.com>
2021-09-20 12:08:18 +0800

    update

--- wordops.sh
+++ wordops.sh
@@ -34,16 +34,15 @@ cd htdocs
 
 # Install plugins and themes
 usermod --shell /bin/bash www-data
-su www-data
-wp language core install zh_TW
-wp language core activate zh_TW
-wp option update timezone_string "Asia/Taipei"
-wp site switch-language zh_TW
-wp plugin install woocommerce
-wp language plugin install woocommerce zh_TW
-wp theme install storefront
-wp language theme install storefront zh_TW
-wp theme activate storefront
+wp language core install zh_TW --allow-root
+wp language core activate zh_TW --allow-root
+wp option update timezone_string "Asia/Taipei" --allow-root
+wp site switch-language zh_TW --allow-root
+wp plugin install woocommerce --allow-root
+wp language plugin install woocommerce zh_TW --allow-root
+wp theme install storefront --allow-root
+wp language theme install storefront zh_TW --allow-root
+wp theme activate storefront --allow-root
 cd wp-content/plugins
 git clone "$repo"
 cd "$plugin_name"
