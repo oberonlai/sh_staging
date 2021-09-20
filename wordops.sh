b5de9696cab0478252c9120592f2b70d371fe70e
Oberon Lai <m615926@gmail.com>
2021-09-20 17:04:43 +0800

    update

--- wordops.sh
+++ wordops.sh
@@ -19,16 +19,6 @@ sudo timedatectl set-timezone Asia/Taipei
 wget -qO wo wops.cc && sudo bash wo --force
 wo stack upgrade --nginx
 wo site create "$staging_url" --wp --user="$staging_user" --pass="$staging_pass" --email="$staging_email" --letsencrypt
-
-# Install Tool
-sudo apt update
-sudo apt -y upgrade
-sudo apt -y autoremove
-
-sudo apt -y install git-all
-sudo apt -y install nodejs npm
-sudo apt -y install composer
-
 wo site cd "$staging_url"
 cd htdocs
 
@@ -43,6 +33,15 @@ wp language plugin install woocommerce zh_TW --allow-root
 wp theme install storefront --allow-root
 wp language theme install storefront zh_TW --allow-root
 wp theme activate storefront --allow-root
+
+# Install Tool
+sudo apt update
+sudo apt -y upgrade
+sudo apt -y autoremove
+sudo apt -y install git-all
+sudo apt -y install nodejs npm
+sudo apt -y install composer
+
 cd wp-content/plugins
 git clone "$repo"
 cd "$plugin_name"
